b9493f9e14d69a661a556e88134322e3b7fa2bbc
Zukhriddin <kz_spiky@mail.ru>
2020-10-17 02:19:12 +0500

    working with state

---
+++ src/Screens/About/helper.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        id: 1,
+        name: "first",
+        color: "blue"
+    },
+
+    {
+        id: 2,
+        name: "second",
+        color: "aqua"
+    },
+
+    {
+        id: 3,
+        name: "third",
+        color: "green"
+    },
+
+    {
+        id: 4,
+        name: "fourth",
+        color: "yellow"
+    },
+
+    {
+        id: 5,
+        name: "fifth",
+        color: "red"
+    }
+]
--- src/Screens/About/index.jsx
+++ src/Screens/About/index.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+//helper
+import rainbow from './helper'
+
+//style
+import './about.css'
 
 export default () => {
+
+    const [bg, setBg] = useState(null)
+
+    const bgColor = (value) => {
+        setBg(value)
+    }
+
     return (
-        <div>asdsa</div>
+
+        // <div>
+        //     <button 
+        //         onClick={() => bgColor(item.color)}
+        //         className={}
+        //     >1</button>
+        //     <button>2</button>
+        //     <button>3</button>
+        //     <button>4</button>
+        //     <button>5</button>
+        // </div>
+
+        <div className="buttonsBg">
+            {
+                rainbow.map((item) => (
+     
+                        <button
+                            key={item.id}
+                            onClick={() => bgColor(item.color)}
+                            className={`${bg === item.color ? 'active' : ''}`}
+                            style={
+                                background = item.color
+                            }
+                        >
+                            {item.name}
+                        </button>
+                   
+                ))
+            }
+        </div>
+
     )
 }
